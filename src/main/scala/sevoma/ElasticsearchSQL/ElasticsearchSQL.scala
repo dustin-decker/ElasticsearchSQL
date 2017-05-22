98e19d929decb903aa77484aed49c0f359edf159
Dustin Decker <sevoma@gmail.com>
2017-05-21 22:17:31 -0500

    reindex data

--- src/main/scala/sevoma/ElasticsearchSQL/ElasticsearchSQL.scala
+++ src/main/scala/sevoma/ElasticsearchSQL/ElasticsearchSQL.scala
@@ -1,6 +1,7 @@
 package sevoma.ElasticsearchSQL
 
-import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.SparkSession
+import org.elasticsearch.spark.sql._
 
 object ElasticsearchSQL extends App {
 
@@ -22,24 +23,28 @@ object ElasticsearchSQL extends App {
     "es.query" ->  "?q=*",
     "es.http.timeout" -> "10s",
     "es.scroll.size" -> "5000",
-    "es.field.read.validate.presence" -> "ignore"
+    "es.read.field.as.array.include" -> "",
+    "es.mapping.date.rich" -> "false",
+    "es.mapping.timestamp" -> "timestamp"
   )
 
   // DataFrame schema automatically inferred
   // Note: You must specify <index-name>/<document-type>
-  val df = sqlContext.read.format(esFormat).options(elasticOptions).load("data-2017.05.21/somedoctype")
+  val df = sqlContext.read.format(esFormat).options(elasticOptions).load("data-2017.05.*/somedoctype")
 
   df.createOrReplaceTempView("esindex")
 
   // Query ES using SQL
-  var res = sqlContext.sql("SELECT uid FROM esindex WHERE proto = 'tcp' AND dst_ip = '127.0.0.1' AND NOT src_ip = '127.0.0.1'")
+  var res = sqlContext.sql("SELECT * FROM esindex WHERE query = 'somequery'")
   println("Elastic count via SQL: " + res.count())
+  res.saveToEs("data-summarized/somedoctype", elasticOptions)
 
-  // Query ES using filter
-  val res2 = df.filter(df("proto").equalTo("tcp")
-    .and(df("dst_ip").equalTo("127.0.0.1"))
-    .and(df("src_ip").notEqual("127.0.0.1")))
-  println("Elastic count via filter: " + res2.count())
+
+//  // Same query against ES using filter
+//  val res2 = df.filter(df("proto").equalTo("tcp")
+//    .and(df("dst_ip").equalTo("127.0.0.1"))
+//    .and(df("src_ip").notEqual("127.0.0.1")))
+//  println("Elastic count via filter: " + res2.count())
 
   sys.exit(0)
 }
